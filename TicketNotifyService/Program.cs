a9801ba78edbae3b45a91ef87ff3b4a4dd1fc32d
Luu Hong <donkeizluv@gmail.com>
2017-10-31 11:48:00 +0700

    Add project files.

---
+++ TicketNotifyService/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.ServiceProcess;
+using TicketNotifyService.Log;
+
+namespace TicketNotifyService
+{
+    static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            if (Environment.UserInteractive || ForceConsole(args))
+            {
+                var service = new TicketNotifyService();
+                LogManager.IsConsole = true;
+                service.ConsoleStart(args);
+            }
+            else
+            {
+                var servicesToRun = new ServiceBase[]
+                {
+                    new TicketNotifyService()
+                };
+                ServiceBase.Run(servicesToRun);
+            }
+        }
+        private static bool ForceConsole(string[] args)
+        {
+            foreach (var a in args)
+            {
+                return string.Compare(a, "-console", true) == 0;
+            }
+            return false;
+        }
+        internal static string ExeDir
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                var uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+    }
+}
---
+++ TicketNotifyService/TicketNotifyService.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using TicketNotifyService.Log;
+
+namespace TicketNotifyService
+{
+    public partial class TicketNotifyService : ServiceBase
+    {
+        internal static string ConfigFileName => string.Format(@"{0}\{1}", Program.ExeDir, CONFIG_FILE_NAME);
+        private const string CONFIG_FILE_NAME = "config.ini";
+        public bool ConsoleMode { get; private set; } = false;
+        private static void Log(string log) => Logger.Log(log);
+        private static readonly ILogger Logger = LogManager.GetLogger(typeof(TicketNotifyService));
+
+        public TicketNotifyService()
+        {
+            InitializeComponent();
+        }
+
+        internal void ConsoleStart(string[] args)
+        {
+            //able to run in 2 modes is cool
+            ConsoleMode = true;
+            Log("Run in console mode....");
+            OnStart(args);
+            Console.ReadLine();
+        }
+        private void TestDapper()
+        {
+            using (IDbConnection db = new MySqlConnection("Server=localhost; database=devdb; UID=dev; password=760119"))
+            {
+                string readSp = "select * from test";
+                var result = db.Query<dynamic>(readSp, commandType: CommandType.Text).ToList();
+                foreach (var row in result)
+                {
+                    var dict = (IDictionary<string, object>)row;
+                    foreach (var item in dict)
+                    {
+                        Console.WriteLine($"{item.Key}:{item.Value}");
+                    }
+                }
+            }
+        }
+        protected override void OnStart(string[] args)
+        {
+            //TestConenction();
+            TestDapper();
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+    }
+}
